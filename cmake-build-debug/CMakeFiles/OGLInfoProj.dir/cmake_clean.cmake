bf3c78ac2cfd8c481e32b12cbae91e85e53e914d
KalokaK <kalokaprivat@gmail.com>
2020-10-29 09:45:26 +0100

    msg

---
+++ cmake-build-debug/CMakeFiles/OGLInfoProj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OGLInfoProj.dir/main.cpp.o"
+  "OGLInfoProj"
+  "OGLInfoProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OGLInfoProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
