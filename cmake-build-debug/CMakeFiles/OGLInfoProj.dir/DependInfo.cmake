c50e38509154817979effd9176c1fe265f6bb506
KalokaK <kalokaprivat@gmail.com>
2020-10-29 14:42:14 +0100

    set up minimal working project, should also run cross platform

--- cmake-build-debug/CMakeFiles/OGLInfoProj.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OGLInfoProj.dir/DependInfo.cmake
@@ -1,8 +1,19 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kaloka/CLionProjects/OGLInfoProj/src/glad.c" "/home/kaloka/CLionProjects/OGLInfoProj/cmake-build-debug/CMakeFiles/OGLInfoProj.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../glfw-3.3.2/include"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/home/kaloka/CLionProjects/OGLInfoProj/main.cpp" "/home/kaloka/CLionProjects/OGLInfoProj/cmake-build-debug/CMakeFiles/OGLInfoProj.dir/main.cpp.o"
   )
@@ -10,10 +21,13 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../glfw-3.3.2/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kaloka/CLionProjects/OGLInfoProj/cmake-build-debug/glfw-3.3.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- cmake-build-debug/CMakeFiles/OGLInfoProj.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/OGLInfoProj.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/OGLInfoProj.dir/main.cpp.o"
+  "CMakeFiles/OGLInfoProj.dir/src/glad.c.o"
   "OGLInfoProj"
   "OGLInfoProj.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/OGLInfoProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
