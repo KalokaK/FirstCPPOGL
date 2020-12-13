6fb172edfdf73f9ffd072c5a5a9a1c923ae3e8c3
kaloka <kalokaprivat@gmail.com>
2020-12-13 11:45:53 +0100

    yes it should work now. mostly.. not really done though.
    Signed-off-by: kaloka <kalokaprivat@gmail.com>

--- src/move.cpp
+++ src/move.cpp
@@ -26,27 +26,7 @@ namespace move {
     }
 
     void GameHandler::textInputListener(char character) {
-        if (gameOver || selectedField < 0) return;
-        chess::piece atSelected = chess::getPiece(selectedField);
-        if (character == char('E') && waitForConvert < 0) { // enter
-            if (selectedField >= 0 && atSelected.colour == turn &&
-                atSelected.type != chess::none && selectedPiece == -1 && chess::canMove(selectedField)) {
-                selectedPiece = selectedField; // select piece at
-                selectedField = -1; // unselect field
-            } else if (selectedField >= 0) { // something is selected
-                moveEventBackend(turn, selectedPiece, selectedField); // tell stuff to move
-                if (atSelected.type == chess::pawn) waitForConvert = selectedField;
-                turn = !turn; // switch turn
-                gameOver = chess::gameOver(turn);
-                displayText->setText(
-                        std::string((bool) turn ? "black" : "white") +
-                        (chess::kingIsThreatened(turn) ? (gameOver ? " lost!" : " is threatened") : "'s turn"));
-                spriteBoard->setTurn(turn); // flip board
-                spriteBoard->highlight.clear(); // no highlights
-                selectedField = -1; // unselect
-                selectedPiece = -1;
-            }
-        } else if (waitForConvert > -1) {
+        if (waitForConvert > -1) {
             switch (character) {
                 case 'Q': // uh... at this point making things stateless in chess would be much easier....
                     chess::setPiece(waitForConvert, chess::piece(chess::queen, chess::getPiece(waitForConvert).colour));
@@ -55,15 +35,50 @@ namespace move {
                     chess::setPiece(waitForConvert, chess::piece(chess::rook, chess::getPiece(waitForConvert).colour));
                     break;
                 case 'K':
-                    chess::setPiece(waitForConvert, chess::piece(chess::knight, chess::getPiece(waitForConvert).colour));
+                    chess::setPiece(waitForConvert,
+                                    chess::piece(chess::knight, chess::getPiece(waitForConvert).colour));
                     break;
                 case 'B':
-                    chess::setPiece(waitForConvert, chess::piece(chess::bishop, chess::getPiece(waitForConvert).colour));
+                    chess::setPiece(waitForConvert,
+                                    chess::piece(chess::bishop, chess::getPiece(waitForConvert).colour));
                     break;
                 default:
                     break;
             }
             waitForConvert = -1;
+        } else if (!gameOver && selectedField > -1) {
+            chess::piece atSelected = chess::getPiece(selectedField);
+            if (character == char('E') && waitForConvert < 0) { // enter
+                if (selectedField >= 0 && atSelected.colour == turn &&
+                    atSelected.type != chess::none && selectedPiece == -1 && chess::canMove(selectedField)) {
+                    selectedPiece = selectedField; // select piece at
+                    selectedField = -1; // unselect field
+                } else if (selectedField >= 0) { // something is selected
+                    moveEventBackend(turn, selectedPiece, selectedField); // tell stuff to move
+                    turn = !turn; // switch turn
+                    if (atSelected.type == chess::pawn) waitForConvert = selectedField;
+                    gameOver = chess::gameOver(turn);
+                    displayText->setText( // (w / b) + (change pawn [or] normal play ->  (lost [or] won) [or] turn )
+                            std::string((waitForConvert > -1) ^ (bool) turn ? "black" : "white") +
+                            // flip if waiting for convert // xor
+                            ((waitForConvert > -1) ? // if converting pawn
+                             std::string(" select [Q]een, [R]ook, [K]night or [B]ishop \n"
+                                         "by pressing the respective key \n for field: ")
+                             + std::to_string(selectedField % 8)
+                             + std::string(", ")
+                             + std::to_string(selectedField / 8)
+                                                   : // else if normal gameplay
+                             (chess::kingIsThreatened(turn) ? // if under threat
+                              (gameOver ? " lost!" : " is threatened")  // either game lost or under threat
+                                                            : // if not under threat -> turn
+                              "'s turn"))
+                    );
+                    spriteBoard->setTurn(turn); // flip board
+                    spriteBoard->highlight.clear(); // no highlights
+                    selectedField = -1; // unselect
+                    selectedPiece = -1;
+                }
+            }
         }
     }
     // wanted to use reified actions... I dont know if this struct is ever used...
